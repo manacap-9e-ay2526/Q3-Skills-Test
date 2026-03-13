4810ca3e48398ca869fef2b876f7743d2a1d615f
manacap-9e-ay2526 <manacap.jy@obmontessori.edu.ph>
2026-03-13 11:24:40 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Declaration of Variable
+const ADFEE = 50.00;
+const TICKET_PRICE = 250.00;
+let tot = subTotal + ADFEE;
+let subTotal= quantity * TICKET_PRICE;
+let quantity = document.getElementById('Number').value;
+
